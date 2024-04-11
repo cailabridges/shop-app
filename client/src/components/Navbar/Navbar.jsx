eb1e760cf9aa7fe0e9ba9a87f0872b665b4fb063
Caila Bridges <bridgescaila@gmail.com>
2024-04-10 22:02:24 -0400

    Edit navbar/footer

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,68 +1,99 @@
 import React, { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import SearchIcon from "@mui/icons-material/Search";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import { Link } from "react-router-dom";
-import "./Navbar.scss"
+import "./Navbar.scss";
 import Cart from "../Cart/Cart";
 import { useSelector } from "react-redux";
 
 const Navbar = () => {
-  const [open,setOpen] = useState(false)
+  const [openMenu, setOpenMenu] = useState(false);
+  const [openCart, setOpenCart] = useState(false);
   const products = useSelector((state) => state.cart.products);
+
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
+  const toggleCart = () => {
+    setOpenCart(!openCart);
+  };
 
   return (
     <div className="navbar">
       <div className="wrapper">
         <div className="left">
           <div className="item">
-            <img src="/img/en.png" alt="" />
-            <KeyboardArrowDownIcon />
+            <Link className="link" to="/products/1">
+              Women
+            </Link>
           </div>
           <div className="item">
-            <span>USD</span>
-            <KeyboardArrowDownIcon />
+            <Link className="link" to="/products/2">
+              Men
+            </Link>
           </div>
           <div className="item">
-            <Link className ="link" to="/products/1">Women</Link>
-          </div>
-          <div className="item">
-            <Link className ="link" to="/products/2">Men</Link>
-          </div>
-          <div className="item">
-            <Link className ="link" to="/products/3">Children</Link>
+            <Link className="link" to="/products/3">
+              Children
+            </Link>
           </div>
         </div>
         <div className="center">
-          <Link className ="link" to="/">LAMASTORE</Link>
+          <Link className="link" to="/">
+            SQRLSTORE
+          </Link>
         </div>
         <div className="right">
-          <div className="item">
-            <Link className ="link" to="/">Homepage</Link>
-          </div>
-          <div className="item">
-            <Link className ="link" to="/">About</Link>
-          </div>
-          <div className="item">
-            <Link className ="link" to="/">Contact</Link>
-          </div>
-          <div className="item">
-            <Link className ="link" to="/">Stores</Link>
+          <div className={`dropdown ${openMenu ? "open" : ""}`}>
+            <div className="item">
+              <Link className="link" to="/">
+                Homepage
+              </Link>
+            </div>
+            <div className="item">
+              <Link className="link" to="/about">
+                About
+              </Link>
+            </div>
+            <div className="item">
+              <Link className="link" to="/contact">
+                Contact
+              </Link>
+            </div>
+            <div className="item">
+              <Link className="link" to="/store">
+                Store
+              </Link>
+            </div>
           </div>
           <div className="icons">
-            <SearchIcon/>
-            <PersonOutlineOutlinedIcon/>
-            <FavoriteBorderOutlinedIcon/>
-            <div className="cartIcon" onClick={()=>setOpen(!open)}>
-              <ShoppingCartOutlinedIcon/>
+            <SearchIcon />
+            <span onClick={toggleMenu}>
+              <MenuIcon />
+            </span>
+            <PersonOutlineOutlinedIcon />
+            <FavoriteBorderOutlinedIcon />
+            <div className="cartIcon" onClick={toggleCart}>
+              <ShoppingCartOutlinedIcon />
               <span>{products.length}</span>
+            </div>
+            {/* <div className="item">
+              <img src="/img/en.png" alt="" />
+              <KeyboardArrowDownIcon />
+            </div> */}
+            <div className="item">
+              <span>USD</span>
+              <KeyboardArrowDownIcon />
             </div>
           </div>
         </div>
       </div>
-      {open && <Cart/>}
+      {openCart && <Cart />}
     </div>
   );
 };
